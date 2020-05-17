d6fe039fe45c9d3d69308b2b00b705aaf4ea3e8e
kalaistark <kalaiselvant0@gmail.com>
2020-05-18 00:49:48 +0530

    added navbar | popular posts | news feed

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(express.static("views"))
+app.use(bodyParser.urlencoded({extended:true}))
+
+var data = ""
+
+app.get('/', (req, res)=>{
+    res.render("index.ejs",{yourName:data})
+})
+app.get('/form', (req, res)=>{
+    res.render("form.ejs")
+})
+app.post('/form', (req, res)=>{
+    data = req.body.your_name
+    res.redirect('/')
+})
+
+app.listen(8080, ()=> console.log("Server fired up"))
