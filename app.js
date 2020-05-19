5c210a2b86d345040d3226169af189a4d1d15af3
kalaistark <kalaiselvant0@gmail.com>
2020-05-19 21:48:54 +0530

    v1

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-var admin = require("firebase-admin");
+var admin = require("firebase-admin")
 var methodOverride = require('method-override')
+var passport = require('passport')
+var localStrategy = require('passport-local')
+var passportLocalMongoose = require('passport-local-mongoose')
+var Admin = require('./models/admin.js')
 
+const app = express()
+
+// passport config
+app.use(require("express-session")({
+    secret : "Winter is coming",
+    resave : false,
+    saveUninitialized : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(Admin.authenticate()));
+passport.serializeUser(Admin.serializeUser());
+passport.deserializeUser(Admin.deserializeUser());
+// 
 var serviceAccount = require("/Users/kalaiselvan/Desktop/20.1/credentials/serviceAccountKey.json");
 
 admin.initializeApp({
@@ -31,19 +49,39 @@ const postData = new mongoose.Schema({
 var User = mongoose.model("User", dataSchema)
 var Post = mongoose.model("Post", postData)
 
-const app = express()
+
 app.use(express.static("views"))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 
-var data = ""
+app.get('/',(req,res)=>{
+    res.render("landing.ejs")
+})
 
-app.get('/', (req, res) => {
+app.post('/register',(req,res)=>{
+    var newUser = new Admin({username:req.body.username})
+    Admin.register(newUser,req.body.password,(err,user)=>{
+        if(err){
+            res.redirect('/')
+        }
+        passport.authenticate("local")(req,res,()=>{
+            res.redirect('/home')
+        })
+    })
+})
+
+app.post('/login',passport.authenticate("local",{
+    successRedirect:"/home",
+    failureRedirect:"/"
+}))
+
+app.get('/home', isLoggedIn,(req, res) => {
     // db.collection("Posts").get().then(snap => {
     //     res.send(snap)
     // })
+    var currentUser = req.user
     Post.find({},(err,Data)=>{
-        res.render('index.ejs',{posts:Data})
+        res.render('index.ejs',{posts:Data,user:currentUser})
     })
 })
 
@@ -57,14 +95,11 @@ app.get('/api', (req, res) => {
         }
     })
 })
-app.get('/form', (req, res) => {
+
+app.get('/form',isLoggedIn,(req, res) => {
     res.render("form.ejs")
 })
-app.get('/:id', (req, res) => {
-    var id = req.params.id
-    console.log(id)
-    res.render('test.ejs', { data: id })
-})
+
 
 app.post('/form', (req, res) => {
     var fireData = {
@@ -83,12 +118,17 @@ app.post('/form', (req, res) => {
         date: req.body.date
     }).save().then(savedData => {
         console.log("data saved", savedData)
-        res.redirect('/')
+        res.redirect('/home')
     }).catch(err => {
         console.log("can't save post")
     })
 })
-app.get('/update/:id',(req,res)=>{
+app.get("/logout",function(req,res){
+    req.logout();
+    // alert("Logged out successfully");
+    res.redirect("/");
+})
+app.get('/update/:id',isLoggedIn,(req,res)=>{
     var id = req.params.id
     Post.findById(id).then(post => {
         res.render('updateform.ejs',{post:post})
@@ -104,17 +144,23 @@ app.post('/update/:id',(req,res)=>{
         category: req.body.category,
         date: req.body.date
     }
-    Post.findByIdAndUpdate(id,updatedData).then(res.redirect('/')).catch(err => console.log(err))
+    Post.findByIdAndUpdate(id,updatedData).then(res.redirect('/home')).catch(err => console.log(err))
     
 })
 
-app.get('/delete/:id', (req,res)=>{
+app.get('/delete/:id', isLoggedIn,(req,res)=>{
     var id = req.params.id
     Post.findByIdAndDelete(id).then(console.log("deleted")).catch(err => console.log(err))
-    res.redirect('/')
+    res.redirect('/home')
 })
-app.get("/*", (req, res) => {
-    res.send("Try valid routes")
-})
+
+
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/");
+}
 
 app.listen(8000, () => console.log("server fired up @8000"))
